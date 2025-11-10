86131625cd3544eb45bf767bdac332118c60acb2
Borja Cano <bordyja@gmail.com>
2025-11-10 21:43:45 +0100

    Initializamos el backend

---
+++ backend/src/main/java/com/sanlugar/sanluapp/SanluappApplication.java
@@ -0,0 +1,13 @@
+package com.sanlugar.sanluapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanluappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanluappApplication.class, args);
+	}
+
+}
